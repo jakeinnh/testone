09e527702434f6539bca3676950ce85247da07e5
Jim Fontaine <jjf@pcswebdesign.com>
2012-12-07 14:51:46 -0500

    Add initial revision.

---
+++ testing.php
@@ -0,0 +1,65 @@
+<?PHP
+/**************************************************************************\ 
+ Copyright 2012 - Positively Creative Solutions, LLC - All rights reserved.
+
+Positively Creative Solutions, LLC forbids, under any circumstances, the unauthorized
+reproduction of this software or use of illegally obtained software.  Making
+illegal copies of this software is prohibited.  Individuals who violate
+copyright law and software licensing agreements may be subject to criminal
+or civil action by the owner of the copyright.
+
+ 1. It is illegal to copy this software, and install this single program for
+ simultaneous use on multiple machines.
+
+ 2. Unauthorized copies of this software may not be used in any way.  This
+ applies even though you yourself may not have made the illegal copy.
+
+ 3. Purchase of the appropriate number of licensed copies of this software is
+ necessary for maintaining your rights to legal, licensed usage.
+
+DISCLAIMER
+
+THIS SOFTWARE IS PROVIDED BY Positively Creative Solutions, LLC "AS IS" AND ANY
+EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED.  IN NO EVENT SHALL Positively Creative Solutions, LLC BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+OR THE INABILITY TO USE THIS SOFTWARE, EVEN IF Positively Creative Solutions, LLC HAS BEEN
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The owner of this original code is Positively Creative Solutions, LLC
+All portions are Copyright � 1989-2011                     
+Positively Creative Solutions, LLC - All Rights Reserved.
+
+$File$
+$DateTime$
+$Revision$
+$Change$
+$Author$
+
+\**************************************************************************/ 
+
+#
+# Purpose of this module:  sample php code
+#
+
+#
+# Insert PHP code below...
+#
+#
+
+echo "testing...";
+
+#
+# Insert PHP code above...
+#
+
+# 
+# Please check that you have no whitespace or empty lines below the closing PHP tag.
+# Whitespace or empty lines below the closing PHP tag may cause a PHP error.
+#
+?>
